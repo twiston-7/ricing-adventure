0347c92e6fcdaddb3bc913e7532591a76416c92e
Twiston <twiston7@proton.me>
2025-10-11 19:38:27 +0200

    Added hyprpaper conf to sh script

--- apply-dotfiles.sh
+++ apply-dotfiles.sh
@@ -4,6 +4,7 @@ mkdir -p ~/.config/hypr
 cp ./dotfiles/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
 
 # Wallpaper stuff
+cp ./dotfiles/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 mkdir -p ~/wallpapers
 cp ./images/wallpaper.png ~/wallpapers/wallpaper.png
 sed -i "s|\$HOME|$HOME|g" ~/.config/hypr/hyprpaper.conf
