31cf82cdd432ed4e10e9921de748a112004a5ff6
Twiston <twiston7@proton.me>
2025-10-11 19:35:55 +0200

    Fixed issue where wallpapers folder wasnt created

--- apply-dotfiles.sh
+++ apply-dotfiles.sh
@@ -4,6 +4,7 @@ mkdir -p ~/.config/hypr
 cp ./dotfiles/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
 
 # Wallpaper stuff
+mkdir -p ~/wallpapers
 cp ./images/wallpaper.png ~/wallpapers/wallpaper.png
 sed -i "s|\$HOME|$HOME|g" ~/.config/hypr/hyprpaper.conf
 
