37705b05b9082e564f516ce9441bb268e1f3ee6d
Twiston <twiston7@proton.me>
2025-10-11 20:07:13 +0200

    Commit

--- update.sh
+++ update.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+
+# Run the dotfiles application
 bash apply-dotfiles.sh
+
+# Reload Hyprland configuration
 hyprctl reload
+
+# Restart hyprpaper
 pkill hyprpaper
 nohup hyprpaper &>/dev/null &
-killall -SIGUSR2 waybar
+
+# Check if waybar is running
+if pgrep -x "waybar" > /dev/null; then
+    # If running, kill it
+    pkill -x waybar
+fi
+
+# Start waybar
+nohup waybar &>/dev/null &
