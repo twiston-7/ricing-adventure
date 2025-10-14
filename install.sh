20e95b86427fb694da12f846edbf1636e0bd5876
Twiston <twiston7@proton.me>
2025-10-14 17:19:42 +0200

    Commit

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ systemctl --user enable pipewire pipewire-pulse wireplumber
 systemctl --user start pipewire pipewire-pulse wireplumber
 
 # Install ohmyzsh
-sh -c "CHSH=yes RUNZSH=yes KEEP_ZSHRC=no OVERWRITE_CONFIRMATION=no $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" CHSH=yes RUNZSH=no KEEP_ZSHRC=no OVERWRITE_CONFIRMATION=no
 
 # Apply dotfiles
 bash apply-dotfiles.sh
